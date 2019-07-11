7695a33aa72a527c9324c48e477e1223c7557019
duesas <50774432+duesas@users.noreply.github.com>
2019-07-11 00:01:01 -0400

    Add files via upload

--- Proyecto/Proyecto/perfil.php
+++ Proyecto/Proyecto/perfil.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>perfil</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="style_home.css">
     <link rel="icon" href="img/logo.ico"> <!-- este codigo es para el favico de la pagina-->
 </head>
